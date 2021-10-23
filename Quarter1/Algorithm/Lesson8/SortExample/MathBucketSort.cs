221d8610dd86029dd207fd1f4bcb325857e948a1
Galushko Andrew <strateg23@gmail.com>
2021-10-23 20:45:07 +0300

    добавил задачу 8-го урока курса по алгоритмам

---
+++ Quarter1/Algorithm/Lesson8/SortExample/MathBucketSort.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeekBrains.Learn.SortExample
+{
+    class MathBucketSort
+    {
+        public void BucketSort(int[] array)
+        {
+            int itemCount = array.Length;
+
+            int xMin = int.MaxValue;
+            int xMax = int.MinValue;
+            // определяем минимальное и максимальное значение в массиве
+            for (int i = 0; i < itemCount; i++)
+            {
+                if (xMin > array[i])
+                {
+                    xMin = array[i];
+                }
+
+                if (xMax < array[i])
+                {
+                    xMax = array[i];
+                }
+            }
+
+            // определяем количество корзин
+            int bucketCount = Math.Min(5, itemCount / 200 + 1);
+
+            int difference = xMax - xMin + 1;
+
+            if (bucketCount > difference)
+            {
+                bucketCount = difference;
+            }
+
+            // шаг для распределения по корзинам
+            int step = difference / bucketCount;
+
+            // сортируем по корзинам
+            // сами корзины
+            List<int[]> buckets = new List<int[]>();
+            for (int i = 0; i < bucketCount; i++)
+            {
+                buckets.Add(new int[itemCount]);
+            }
+
+            // количество помещенных в них элементов и граничные значения чисел
+            // 0 - количество элементов
+            // 1 - минимальное значение включительно
+            // 2 - максимальное значение не включительно
+            int[,] bucketSize = new int[bucketCount, 3];
+
+            // определяем граничные значения в корзинах
+            for (int i = 0; i < bucketCount; i++)
+            {
+                // min
+                bucketSize[i, 1] = xMin + step * i;
+                // max
+                if (i == bucketCount - 1)
+                {
+                    bucketSize[i, 2] = xMax + 1;
+                }
+                else
+                {
+                    bucketSize[i, 2] = xMin + step * (i + 1);
+                }
+            }
+
+            // размещение элементов в корзины
+            for (int i = 0; i < itemCount; i++)
+            {
+                for (int j = 0; j < bucketCount; j++)
+                {
+                    int value = array[i];
+                    if (value >= bucketSize[j, 1] &&
+                        value < bucketSize[j, 2])
+                    {
+                        buckets[j][bucketSize[j, 0]] = value;
+                        bucketSize[j, 0]++;
+                    }
+                }
+            }
+
+            //int[] result = new int[itemCount];
+            for (int i = 0; i < bucketCount; i++)
+            {
+                if (bucketSize[i, 2] - bucketSize[i, 1] != 0)
+                {
+                    var t = buckets[i];
+                    QuickSort(t, 0, bucketSize[i, 0] - 1);
+                }
+            }
+
+            // склеиваем корзины
+            int index = 0;
+            for (int i = 0; i < bucketCount; i++)
+            {
+                for (int j = 0; j < bucketSize[i, 0]; j++)
+                {
+                    array[index] = buckets[i][j];
+                    index++;
+                }
+            }
+
+            //return result;
+        }
+
+        private void QuickSort(int[] array, int first, int last)
+        {
+            int i = first, j = last, x = array[(first + last) / 2];
+
+            do
+            {
+                while (array[i] < x)
+                    i++;
+                while (array[j] > x)
+                    j--;
+
+                if (i <= j)
+                {
+                    if (array[i] > array[j])
+                    {
+                        var tmp = array[i];
+                        array[i] = array[j];
+                        array[j] = tmp;
+                    }
+
+                    i++;
+                    j--;
+                }
+            } while (i <= j);
+
+            if (i < last)
+                QuickSort(array, i, last);
+            if (first < j)
+                QuickSort(array, first, j);
+        }
+
+    }
+}
---
+++ Quarter1/Algorithm/Lesson8/SortExample/Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace GeekBrains.Learn.SortExample
+{
+    class Test
+    {
+        /// <summary>
+        /// Генерация массива со значениями
+        /// </summary>
+        /// <param name="n">количество элементов</param>
+        /// <returns></returns>
+        private int[] ArrayGenerate(int n)
+        {
+            int[] arr = new int[n];
+            Random rnd = new();
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = rnd.Next(1, n);
+            }
+
+            return arr;
+        }
+
+        public void Start()
+        {
+            int count = 1000;
+            const int formatLength = 5;
+            var arr = ArrayGenerate(count);
+            MathBucketSort mathBucketSort = new();
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"{i + 1, formatLength} - {arr[i], formatLength}");
+            }
+            Console.WriteLine("----------------------------");
+
+            mathBucketSort.BucketSort(arr);
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"{i + 1, formatLength} - {arr[i], formatLength}");
+            }
+        }
+    }
+}
