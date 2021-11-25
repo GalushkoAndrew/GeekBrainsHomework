8a23decb71c22359a6c07b4232e1ba1fce896259
Galushko Andrew <strateg23@gmail.com>
2021-11-25 19:44:37 +0300

    Добавил решение задач 3 урока курса ООП

---
+++ Quarter1/OOP/Lesson2/BankAccount/BankAccount/Account.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace GeekBrains.Learn.BankAccount
+{
+    /// <summary>
+    /// Банковский счет
+    /// </summary>
+    public sealed class Account : IAccount
+    {
+        /// <summary>
+        /// Баланс
+        /// </summary>
+        private float _balance = 0;
+
+        /// <summary>
+        /// Тип банковского счета
+        /// </summary>
+        private AccountTypes _accountType = AccountTypes.Savings;
+
+        /// <summary>
+        /// Номер счета
+        /// </summary>
+        private static int _number = 0;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public Account() : this(0, AccountTypes.Savings)
+        {
+        }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public Account(float balance) : this(balance, AccountTypes.Savings)
+        {
+        }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public Account(AccountTypes accountType) : this(0, accountType)
+        {
+        }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public Account(float balance, AccountTypes accountType)
+        {
+            GenerateNewNumber();
+            Balance = balance;
+            AccountType = accountType;
+        }
+
+        /// <summary>
+        /// Номер счета
+        /// </summary>
+        public static int Number
+        {
+            get { return _number; }
+            private set { _number = value; }
+        }
+
+        /// <summary>
+        /// Баланс
+        /// </summary>
+        private float Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+
+        /// <inheritdoc/>
+        public AccountTypes AccountType
+        {
+            get { return _accountType; }
+            set { _accountType = value; }
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return $"Тип счета: {AccountType}, № {Number}, баланс: {Balance}";
+        }
+
+        /// <inheritdoc/>
+        public bool GetMoney(float value)
+        {
+            if (Balance >= value)
+            {
+                Balance -= value;
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <inheritdoc/>
+        public bool PutMoney(float value)
+        {
+            try
+            {
+                Balance += value;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <inheritdoc/>
+        public bool GetMoneyFromAccount(Account accountFrom, float value)
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Попытка взлома через перевод отрицательной суммы. Операция отменена.");
+                return false;
+            }
+
+            float transferedValue;
+            if (accountFrom.GetMoney(value))
+            {
+                transferedValue = value;
+            }
+            else
+            {
+                Console.WriteLine("Не удалось снять сумму со счета акаунта-источника");
+                return false;
+            }
+
+            if (PutMoney(transferedValue))
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Не удалось зачислить снятую сумму на счет акаунта-получателя");
+                if (!accountFrom.PutMoney(transferedValue))
+                {
+                    Console.WriteLine("Не удалось вернуть снятую сумму на счет акаунта-источника. Деньги пропали, обратитесь в банк.");
+                }
+
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Генерирует номер счета
+        /// </summary>
+        private static void GenerateNewNumber()
+        {
+            Number++;
+        }
+    }
+}
--- Quarter1/OOP/Lesson2/BankAccount/BankAccount/Tests.cs
+++ Quarter1/OOP/Lesson2/BankAccount/BankAccount/Tests.cs
@@ -6,40 +6,26 @@ namespace GeekBrains.Learn.BankAccount
     {
         public void Start()
         {
-            Account_v1 account_V1 = new();
-            Console.WriteLine(account_V1.GetType());
-            account_V1.SetAccountType(AccountTypes.Currency);
-            account_V1.SetNumber(321);
-            account_V1.SetBalance(100500);
-            Console.WriteLine(account_V1.ToString());
-            Console.WriteLine("--------------------");
-
-            Account_v2 account_V2 = new();
-            Console.WriteLine(account_V2.GetType());
-            account_V2.SetAccountType(AccountTypes.Currency);
-            account_V2.GenerateNewNumber();
-            account_V2.SetBalance(100502);
-            Console.WriteLine(account_V2.ToString());
-            Console.WriteLine("--------------------");
-
-            Account_v3 account_V3 = new(12.5F, AccountTypes.Cumulative);
-            Console.WriteLine(account_V3.GetType());
-            Console.WriteLine(account_V3.ToString());
+            Account account = new(12.5F, AccountTypes.Cumulative);
+            Console.WriteLine(account.GetType());
+            Console.WriteLine(account.ToString());
             Console.WriteLine("----");
 
-            Account_v3 account_V3_2 = new(500.1F, AccountTypes.Savings);
-            Console.WriteLine(account_V3_2.ToString());
-            account_V3_2.PutMoney(100500);
-            Console.WriteLine(account_V3_2.ToString());
-            account_V3_2.GetMoney(15.18F);
-            Console.WriteLine(account_V3_2.ToString());
-            Console.WriteLine(account_V3_2.GetMoney(500000) ? "Снятие со счета успешно" : "Снятие со счета не успешно");
-            Console.WriteLine(account_V3_2.ToString());
-            account_V3_2.Balance = 882255;
-            Console.WriteLine(account_V3_2.ToString());
-            account_V3_2.AccountType = AccountTypes.Currency;
-            Console.WriteLine(account_V3_2.ToString());
+            Account account2 = new(500.1F, AccountTypes.Savings);
+            Console.WriteLine(account2.ToString());
+            account2.PutMoney(100500);
+            Console.WriteLine(account2.ToString());
+            account2.GetMoney(15.18F);
+            Console.WriteLine(account2.ToString());
+            Console.WriteLine(account2.GetMoney(500000) ? "Снятие со счета успешно" : "Снятие со счета не успешно");
+            Console.WriteLine(account2.ToString());
+            account2.AccountType = AccountTypes.Currency;
+            Console.WriteLine(account2.ToString());
             Console.WriteLine("----");
+
+            account2.GetMoneyFromAccount(account, 1F);
+            Console.WriteLine(account.ToString());
+            Console.WriteLine(account2.ToString());
         }
     }
 }
