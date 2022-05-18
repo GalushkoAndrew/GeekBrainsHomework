eb853c111cf2296ff2fd0a360d21c5790441a181
Galushko Andrew <strateg23@gmail.com>
2022-05-18 10:30:05 +0300

    урок 7 курса ASP.NET Core Веб Приложение

--- Quarter2/WebApiNetCore/Lesson2/TimeSheets/Domain/Invoice.cs
+++ Quarter2/WebApiNetCore/Lesson2/TimeSheets/Domain/Invoice.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using GeekBrains.Learn.TimeSheets.Domain.Base;
 
 namespace GeekBrains.Learn.TimeSheets.Domain
@@ -18,5 +20,29 @@ namespace GeekBrains.Learn.TimeSheets.Domain
         /// Specification
         /// </summary>
         public virtual ICollection<InvoiceRow> Rows { get; set; }
+
+        /// <summary>
+        /// Invoice initialize
+        /// </summary>
+        public void Create()
+        {
+            Date = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Add sheets to invoice
+        /// </summary>
+        public void IncludeSheets(ICollection<InvoiceRow> sheets)
+        {
+            if (Rows == null) {
+                Rows = new Collection<InvoiceRow>();
+            }
+
+            foreach (var sheet in sheets) {
+                if (!Rows.Any(x => x.Id == sheet.Id)) {
+                    Rows.Add(sheet);
+                }
+            }
+        }
     }
 }
