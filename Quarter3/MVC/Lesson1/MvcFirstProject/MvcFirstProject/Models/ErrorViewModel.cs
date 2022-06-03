0de425f68031d30ee54595e9581360780a53ceb2
Galushko Andrew <strateg23@gmail.com>
2022-06-03 15:18:27 +0300

    Урок 1 курса ASP.NET MVC Core

---
+++ Quarter3/MVC/Lesson1/MvcFirstProject/MvcFirstProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcFirstProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
