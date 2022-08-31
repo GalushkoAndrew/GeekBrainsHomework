cd784eeff9614fe619a25d63a0411d3ca88f8c8c
Galushko Andrew <strateg23@gmail.com>
2022-08-31 20:18:28 +0300

    урок 4 курса Безопасная разработка

---
+++ Quarter4/SecurityDevelopment/Lesson4/SeminarFramework/CacheProvider.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SeminarFramework
+{
+    public class CacheProvider
+    {
+
+        static byte[] additionalEntropy = { 5, 3, 7, 1, 5 };
+
+        public void CacheConnections(List<ConnectionString> connections)
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<ConnectionString>));
+                MemoryStream memoryStream = new MemoryStream();
+                XmlWriter xmlWriter = new XmlTextWriter(memoryStream, Encoding.UTF8);
+                xmlSerializer.Serialize(xmlWriter, connections);
+                byte[] protectedData = Protect(memoryStream.ToArray());
+                File.WriteAllBytes($"{AppDomain.CurrentDomain.BaseDirectory}data.protected", protectedData);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Serialize data error.");
+            }
+        }
+
+        public List<ConnectionString> GetConnectionFromCache()
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<ConnectionString>));
+                byte[] protectedData = File.ReadAllBytes($"{AppDomain.CurrentDomain.BaseDirectory}data.protected");
+                byte[] data = Unprotect(protectedData);
+                return (List<ConnectionString>)xmlSerializer.Deserialize(new MemoryStream(data));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Deserialize data error.");
+                return null;
+            }
+        }
+
+        private byte[] Protect(byte[] data)
+        {
+            try
+            {
+                return ProtectedData.Protect(data, additionalEntropy, DataProtectionScope.LocalMachine);
+            }
+            catch(CryptographicException e)
+            {
+                Console.WriteLine("Protect error.");
+               return null;
+            }
+        }
+
+        private byte[] Unprotect(byte[] data)
+        {
+            try
+            {
+                return ProtectedData.Unprotect(data, additionalEntropy, DataProtectionScope.LocalMachine);
+            }
+            catch(CryptographicException e)
+            {
+                Console.WriteLine($"Unprotect error.\n{e.Message}");
+                return null;
+            }
+        }
+
+
+    }
+}
---
+++ Quarter4/SecurityDevelopment/Lesson4/SeminarFramework/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SeminarFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            Console.Title = Properties.Settings.Default.AppName1;
+#else
+            Console.Title = Properties.Settings.Default.AppName2;
+#endif
+
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.Fio) || Properties.Settings.Default.Age <= 0)
+            {
+                Console.Write("Введите ФИО: ");
+                Properties.Settings.Default.Fio = Console.ReadLine();
+
+                Console.Write("Укажите ваш возраст: ");
+                if (int.TryParse(Console.ReadLine(), out int age))
+                {
+                    Properties.Settings.Default.Age = age;
+                }
+                else
+                {
+                    Properties.Settings.Default.Age = 0;
+                }
+                Properties.Settings.Default.Save();
+            }
+
+            Console.WriteLine($"ФИО: {Properties.Settings.Default.Fio}");
+            Console.WriteLine($"Возраст: {Properties.Settings.Default.Age}");
+
+            // %USERPROFILE%\AppData\Local\
+
+            /*ConnectionString connectionString1 = new ConnectionString
+            {
+                DatabaseName = "Database1",
+                Host = "localhost",
+                Password = "12345",
+                UserName = "User1"
+            };
+
+            ConnectionString connectionString2 = new ConnectionString
+            {
+                DatabaseName = "Database2",
+                Host = "localhost",
+                Password = "54321",
+                UserName = "User2"
+            };
+
+            List<ConnectionString> connections = new List<ConnectionString>();
+            connections.Add(connectionString1);
+            connections.Add(connectionString2);
+            CacheProvider cacheProvider = new CacheProvider();
+            cacheProvider.CacheConnections(connections);*/
+
+            CacheProvider cacheProvider = new CacheProvider();
+            List<ConnectionString> connections = cacheProvider.GetConnectionFromCache();
+
+            foreach (var connection in connections)
+            {
+                Console.WriteLine($"{connection.Host} {connection.DatabaseName} {connection.UserName} {connection.Password}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
