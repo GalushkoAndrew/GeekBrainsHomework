2dbe82813f9812e33184753430a8a18d1cf8c01c
Galushko Andrew <strateg23@gmail.com>
2022-11-07 14:56:03 +0300

    урок 3 курса Работа с протоколами SOAP, REST и gRPC

---
+++ Quarter4/GRPC/Lesson3/ClinicService/ClinicClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using ClinicServiceNamespace;
+using Grpc.Net.Client;
+using static ClinicServiceNamespace.ClinicService;
+
+namespace ClinicClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppContext.SetSwitch(
+                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            using var channel = GrpcChannel.ForAddress("http://localhost:5001");
+            ClinicServiceClient clinicServiceClient = new ClinicServiceClient(channel);
+
+            var createClientResponse = clinicServiceClient.CreateClinet(new CreateClientRequest
+            {
+                Document = "DOC34 445",
+                FirstName = "Станислав",
+                Patronymic = "Антонович",
+                Surname = "Байраковский"
+            });
+
+            if (createClientResponse.ErrCode == 0)
+            {
+                Console.WriteLine($"Client #{createClientResponse.ClientId} created successfully.");
+            }
+            else
+            {
+                Console.WriteLine($"Create client error.\nErrorCode: {createClientResponse.ErrCode}\nErrorMessage: {createClientResponse.ErrMessage}");
+            }
+
+            var getClientResponse = clinicServiceClient.GetClients(new GetClientsRequest());
+
+            if (createClientResponse.ErrCode == 0)
+            {
+                Console.WriteLine("Clients");
+                Console.WriteLine("=======\n");
+
+                foreach (var client in getClientResponse.Clients)
+                {
+                    Console.WriteLine($"#{client.ClientId} {client.Document} {client.Surname} {client.FirstName} {client.Patronymic}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Get clients error.\nErrorCode: {getClientResponse.ErrCode}\nErrorMessage: {getClientResponse.ErrMessage}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Quarter4/GRPC/Lesson3/ClinicService/ClinicService.Data/ClinicServiceDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicService.Data
+{
+    public class ClinicServiceDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Consultation> Consultations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Consultation>().HasOne(p => p.Pet).WithMany(b => b.Consultations).HasForeignKey(p => p.PetId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+        }
+
+        public ClinicServiceDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
